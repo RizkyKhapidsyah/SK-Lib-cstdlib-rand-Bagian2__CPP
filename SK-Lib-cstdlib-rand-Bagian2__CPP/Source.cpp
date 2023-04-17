6e09db537b346d401d3a41d5e8a928cf7e9b45b1
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-04-17 20:05:46 +0700

    Initial commit

---
+++ SK-Lib-cstdlib-rand-Bagian2__CPP/Source.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include <conio.h>
+
+/*
+    Source by GeeksForGeeks (https://www.geeksforgeeks.org)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        cout << rand() << " ";
+    }
+
+    _getch();
+    return 0;
+}
